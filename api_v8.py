7b42d8bebece1cc2b368cf416beeb18a2bc76561
tm <femis@users.noreply.github.com>
2024-12-29 11:45:05 +0800

    新增下载失败重试

--- api_v8.py
+++ api_v8.py
@@ -12,12 +12,17 @@ from pydub import AudioSegment
 
 import sys
 import os
+import logging
+
+# 配置日志记录器
+logging.basicConfig(level=logging.INFO, format='%(asctime)s - %(levelname)s - %(message)s')
+logger = logging.getLogger(__name__)
 
 # 启用并行推理
 os.environ["TOKENIZERS_PARALLELISM"] = "true"  # 或 "true"
 # 获取项目根目录
 project_root = os.path.abspath(os.path.join(os.path.dirname(__file__), './GPT_SoVITS'))
-print("追加项目根目录:", project_root)
+logger.info(f"追加项目根目录: {project_root}")
 
 # 确保项目根目录在 sys.path 中
 if project_root not in sys.path:
@@ -31,7 +36,7 @@ parser = argparse.ArgumentParser(description='文字转语言服务')
 # 获取当前目录 绝对地址
 temp_dir = os.path.join(os.path.dirname(os.path.abspath(__file__)), "TEMP")
 device = torch.device('cuda' if torch.cuda.is_available() else 'cpu')
-print('当前使用的设备是：', device)
+logger.info(f'当前使用的设备是：{device}')
 
 tts_config_cache = {}  # 缓存 TTS_Config 对象
 tts_init_cache = {}
@@ -42,17 +47,17 @@ def get_tts_config(req):
     global tts_config_cache  # 使用global声明来访问外部的tts_config_cache
     tts_infer_yaml_path = req.get("tts_infer_yaml_path", "GPT_SoVITS/configs/tts_infer.yaml")
     unique_id = req.get("unique_id", "123456")
-    print(f"本地请求的唯一Id: {unique_id}")
+    logger.info(f"本地请求的唯一Id: {unique_id}")
     # 检查缓存大小，如果超过20则清空缓存
     if len(tts_config_cache) > 20:
-        print("缓存大小超过10，清空缓存...")
+        logger.info("缓存大小超过10，清空缓存...")
         for instance_id, instance in list(tts_config_cache.items()):
-            print(f"删除索引： {instance_id}")
+            logger.info(f"删除索引： {instance_id}")
             del tts_config_cache[instance_id]
 
     # 由于我们可能刚清空了缓存，现在再次检查unique_id是否在缓存中
     if unique_id not in tts_config_cache:
-        print(f"从文件加载配置并添加到缓存中: {tts_infer_yaml_path}")
+        logger.info(f"从文件加载配置并添加到缓存中: {tts_infer_yaml_path}")
         # 假设TTS_Config是一个类，它接受yaml文件路径作为参数来加载配置
         tts_config_cache[unique_id] = TTS_Config(tts_infer_yaml_path)
 
@@ -60,49 +65,57 @@ def get_tts_config(req):
     return tts_config_cache[unique_id]
 
 # 加载TTS缓存
-def init_tts (req) :
+def init_tts(req):
     global tts_init_cache
     unique_id = req.get("unique_id", "123456")
     tts_init_max = req.get("tts_init_max", 5)
     tts_config = get_tts_config(req)
-    print(f"将TTS实例当前已存储： {len(tts_init_cache)}， 最大允许： {tts_init_max} 条")
+    logger.info(f"将TTS实例当前已存储： {len(tts_init_cache)}， 最大允许： {tts_init_max} 条")
     if len(tts_init_cache) > tts_init_max:
-        print(f"初始化TTS超过{tts_init_max}，清空缓存中...")
+        logger.info(f"初始化TTS超过{tts_init_max}，清空缓存中...")
         for instance_id, instance in list(tts_init_cache.items()):
             # 需要清理释放缓/
             instance.stop()
             instance.empty_cache()
-            print(f"删除索引： {instance_id}")
+            logger.info(f"删除索引： {instance_id}")
             del tts_init_cache[instance_id]
 
     if unique_id not in tts_init_cache:
-        print(f"将TTS实例增加到缓存中...{unique_id}")
+        logger.info(f"将TTS实例增加到缓存中...{unique_id}")
         # 假设TTS_Config是一个类，它接受yaml文件路径作为参数来加载配置
         tts_init_cache[unique_id] = TTS(tts_config)
 
     return tts_init_cache[unique_id]
 
 # 下载文件
-def download_file(url:str = ''):
+def download_file(url: str = '', max_retries: int = 3, delay: int = 5):
     # 判断文件是 http 开头才执行, 否则直接返回
     if not url.startswith('http'):
         return url
     # 发送GET请求
-    try:
-        response = requests.get(url)
-        # 确保请求成功
-        if response.status_code == 200:
-            with tempfile.NamedTemporaryFile(delete=False, suffix='.mp3', dir=temp_dir) as temp_file:
-                temp_file.write(response.content)
-                temp_file_path = temp_file.name  # 获取临时文件的路径
-            print(f'文件已成功下载到系统缓存目录: {temp_file_path}')
-            return temp_file_path
-        else:
-            print('下载失败，状态码:', response.status_code)
-            return ""
-    except Exception as e:
-        print('下载失败')
-        return ""
+    for attempt in range(max_retries):
+        try:
+            logger.info(f'正在下载文件: {url}')
+            response = requests.get(url, verify=False)
+            # 确保请求成功
+            if response.status_code == 200:
+                with tempfile.NamedTemporaryFile(delete=False, suffix='.mp3', dir=temp_dir) as temp_file:
+                    temp_file.write(response.content)
+                    temp_file_path = temp_file.name  # 获取临时文件的路径
+                logger.info(f'文件已成功下载到系统缓存目录: {temp_file_path}')
+                return temp_file_path
+            else:
+                logger.info(f'下载失败，状态码: {response.status_code}')
+        except Exception as e:
+            logger.info(f'下载失败: {e}')
+
+        # 如果不是最后一次尝试，则等待一段时间后重试
+        if attempt < max_retries - 1:
+            logger.info(f'尝试 {attempt + 1} 失败，将在 {delay} 秒后重试...')
+            time.sleep(delay)
+    # 如果所有尝试都失败，则返回空字符串
+    logger.info('所有重试均失败')
+    return ""
 
 # 将mp3转wav
 def convert_mp3_to_wav(mp3_file_path, wav_file_path):
@@ -110,11 +123,11 @@ def convert_mp3_to_wav(mp3_file_path, wav_file_path):
     audio = AudioSegment.from_mp3(mp3_file_path)
     # 导出为WAV文件
     audio.export(wav_file_path, format="wav")
-    print('转WAV成功', wav_file_path)
+    logger.info('转WAV成功', wav_file_path)
     return wav_file_path
 
 # 将wav转mp3
-def convert_wav_to_mp3(wav_file_path, mp3_file_path ):
+def convert_wav_to_mp3(wav_file_path, mp3_file_path):
     # 加载WAV文件
     wav_file = AudioSegment.from_wav(wav_file_path)
     # 导出为MP3文件
@@ -122,15 +135,15 @@ def convert_wav_to_mp3(wav_file_path, mp3_file_path ):
     return mp3_file_path
 
 # 获取临时文件
-def get_temp_file(ext:str = '.mp3'):
+def get_temp_file(ext: str = '.mp3'):
     output_file = tempfile.NamedTemporaryFile(delete=False, suffix=ext, dir=temp_dir).name
-    print(f"临时文件路径: {output_file}")
+    logger.info(f"临时文件路径: {output_file}")
     return output_file
 
 # 初始化TTs方法
 def tts_handle(req: dict):
     # 打印传入的配置信息
-    print(f"传入的配置是: {req}")
+    logger.info(f"传入的配置是: {req}")
     # 保存到本地的音频
     output_file = get_temp_file()
 
@@ -143,7 +156,7 @@ def tts_handle(req: dict):
         sr, audio_data = next(tts_generator)
         # 保存音频到本地文件
         sf.write(output_file, audio_data, sr)
-        print(f"音频已保存到: {output_file}")
+        logger.info(f"音频已保存到: {output_file}")
         file_name = os.path.basename(output_file)
         return {
             "path": output_file,
@@ -153,23 +166,25 @@ def tts_handle(req: dict):
         }
     except Exception as e:
         # 如果在处理请求过程中发生异常，打印错误信息并返回一个空响应对象
-        print(f"生成失败: {str(e)}")
+        logger.error(f"生成失败: {str(e)}")
         return {
             "path": output_file,
             "success": 0,
             "msg": str(e)
         }
+
 # 获取当前时间
 def get_time():
     # 打印格式化的当前日期和时间
     formatted_now = datetime.now().strftime("%Y年%m月%d日 %H时%M分%S秒")
     return formatted_now
+
 @app.route('/make', methods=['POST'])
 def enter():
     global is_makeing
     start_time = time.time()
     is_makeing = 1
-    print(f"即将开始制作，当前时间：{get_time()}")
+    logger.info(f"即将开始制作，当前时间：{get_time()}")
 
     json = request.json
     text = json.get('text', '早知他来，我就不来了')
@@ -191,8 +206,8 @@ def enter():
     yaml_path = json.get('yaml_path', 'GPT_SoVITS/configs/daiyu.yaml')
     unique_id = json.get('unique_id', '123456')
     seed = json.get('seed', -1)
-    batch_size = json.get('batch_size',20) # 批次大小
-    tts_init_max = json.get('tts_init_max', 5) # 最大缓存个数;
+    batch_size = json.get('batch_size', 20)  # 批次大小
+    tts_init_max = json.get('tts_init_max', 5)  # 最大缓存个数;
     result = tts_handle({
         "text": text,  # 待合成的文本内容
         "text_lang": "zh",  # 待合成文本的语言。
@@ -220,8 +235,8 @@ def enter():
         "tts_init_max": int(tts_init_max)
     })
     end_time = time.time()
-    print(f"制作请求完成; 耗时：{end_time - start_time}秒，当前时间：{get_time()}")
-    print(f"生成结果: {result}")
+    logger.info(f"制作请求完成; 耗时：{end_time - start_time}秒，当前时间：{get_time()}")
+    logger.info(f"生成结果: {result}")
     is_makeing = 0
     return jsonify(result)
 
@@ -246,6 +261,7 @@ def download():
         # 从本地读取文件并且输出
         return send_file(full_path, as_attachment=True)
     except Exception as e:
+        logger.error(f"下载文件时发生异常: {str(e)}")
         return jsonify({'error': str(e)}), 500
 
 @app.route('/', methods=['GET'])
@@ -257,20 +273,20 @@ def find():
         config_len = len(tts_config_cache)
         config_len_keys = tts_config_cache.keys()
         result = {
-            'tts_len' : {
-                'count' : tts_len,
-                'list' : list(tts_len_keys)
-            },      # 实例的个数
+            'tts_len': {
+                'count': tts_len,
+                'list': list(tts_len_keys)
+            },  # 实例的个数
             'config_len': {
                 'count': config_len,
-                'list':  list(config_len_keys)
-            }, # 配置文件个数
+                'list': list(config_len_keys)
+            },  # 配置文件个数
             'is_makeing': is_makeing  # 是否制作中
         }
-        print(f"查询结果: {result}")
+        logger.info(f"查询结果: {result}")
         return jsonify(result)
     except Exception as e:
-        app.logger.error(f"处理 / 请求时发生异常: {e}")  # 记录异常信息
+        logger.error(f"处理 / 请求时发生异常: {e}")  # 记录异常信息
         return jsonify({"error": "处理请求时发生内部错误"}), 500  # 返回错误响应
 
 if __name__ == '__main__':
